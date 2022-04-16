c2909c2cfb813f84c8138c18fd0cc9e88115fec8
vova7878 <v7878@github.com>
2022-04-16 10:08:57 +0700

    Revert "Change inheritance to private"
    This reverts commit 8d4f8d8733f53b0fc28c7f5878fe7eff5e63ab09.

--- ByteBuffer.hpp
+++ ByteBuffer.hpp
@@ -116,7 +116,7 @@ namespace JIO {
     };
 
     template<>
-    class ByteBuffer <true> : private ByteBuffer<false> {
+    class ByteBuffer <true> : public ByteBuffer<false> {
     private:
         size_t _position;
 
@@ -145,7 +145,6 @@ namespace JIO {
         using ByteBuffer<false>::put;
         using ByteBuffer<false>::getObject;
         using ByteBuffer<false>::putObject;
-        using ByteBuffer<false>::operator[];
 
         inline void get(void *dst, size_t length) {
             size_t tmp = _position;
