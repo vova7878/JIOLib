9902877219610255c508bb8af8782c4269784be1
vova7878 <vova7878@users.noreply.github.com>
2022-09-17 22:26:14 +0700

    Vector: add mix, clamp and remap functions

--- math/Vector.hpp
+++ math/Vector.hpp
@@ -421,6 +421,34 @@ constexpr inline Vector<T1, size>& operator op(                   \
     }
 
     template<typename T, size_t size>
+    constexpr inline Vector<T, size> clamp(const Vector<T, size> &v, T lo, T hi) {
+        Vector<T, size> out(p_v_unused());
+        for (size_t i = 0; i < size; i++) {
+            T value = v[i];
+            out[i] = value < lo ? lo : (value > hi ? hi : value);
+        }
+        return out;
+    }
+
+    template<typename T1, size_t size, typename T2>
+    constexpr inline Vector<T1, size> mix(
+            const Vector<T1, size> &v1, const Vector<T1, size> &v2, T2 m) {
+        T1 k1 = std::clamp(T1(m), T1(0), T1(1)), k2 = T1(1) - k1;
+        Vector<T1, size> out(p_v_unused());
+        for (size_t i = 0; i < size; i++) {
+            out[i] = v1[i] * k2 + v2[i] * k1;
+        }
+        return out;
+    }
+
+    template<typename T, size_t size>
+    constexpr inline Vector<T, size> remap(const Vector<T, size> &v,
+            T lo_in, T hi_in, T lo_out, T hi_out) {
+        return (clamp(v, lo_in, hi_in) - lo_in) *
+                (hi_out - lo_out) / (hi_in - lo_in) + lo_out;
+    }
+
+    template<typename T, size_t size>
     constexpr inline T length(const Vector<T, size> &v) {
         return sqrt(dot(v));
     }
